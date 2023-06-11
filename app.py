ab03fba570dfaaefad95d259c93a980855c7d5cf
akif <muhammedakifshariff@gmail.com>
2023-06-11 23:26:28 +0530

    modified hello world to palindrome string

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('hello wahab')
+a='radar'
+b='madhu'
+print(type(a),type(b))
+
+ # Check - palindrome
+
+print(a==a[::-1]) # 'radar' == 'radar' - True
+print(b==b[::-1]) # 'madhu' == 'uhdam' - False
